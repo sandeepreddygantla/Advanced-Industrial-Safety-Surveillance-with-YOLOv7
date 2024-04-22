aa01f404a76b5cec25d3d79188061bcd3057f3f4
sandeepreddygantla <reddysandeep0904@gmail.com>
2024-04-22 18:32:42 -0500

    updated with Deployment

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'isd',
     version= '0.0.0',
-    author= 'Bappy',
-    author_email= 'boktiar@ineuron.ai',
+    author= 'SandeepReddy',
+    author_email= 'gantlasandeep98@gmail.com',
     packages= find_packages(),
     install_requires = []
 
